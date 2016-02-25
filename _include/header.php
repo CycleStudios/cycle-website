da55f29df70c4461868a4182a9cb7d0b932a2166
Harry Copeman <harry.copeman@gmail.com>
2016-02-25 12:06:15 +0000

    First Push

---
+++ _include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
+    <link rel="shortcut icon" href="/favicon.png"/>
+
+  	<title><?php echo $title; ?></title>
+  	<link href='/_core/css/style.css' rel='stylesheet'>
+
+    <script src="https://use.typekit.net/vve7mtx.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+    </script>
+
+</head>
+
+    <body id="<?php echo $id; ?>">
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+$title = "Cycle Studios";
+include ("_include/header.php");
+?>
+
+
+<section class="introduction">
+	<div class="span">
+		<div class="loop-12">
+			<div class="loop-12">
+				<h1><strong>Cycle Studios</strong> is a small, established team helping people invent, build, and launch their next product or venture throught beautiful design and proccess.</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="work">
+	<div class="span">
+		<div class="loop-12">
+			<div class="loop-12">
+				<h1>We've worked with...<br><strong>Dropbox, Facebook, Mailbox, John Deer, Switch, EUFA, SeatGeek, C4Di</strong> and more.</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="contact">
+	<div class="span">
+		<div class="loop-12">
+			<div class="loop-12">
+				<h1><strong>We're currently seeker longer contracts and are happy to be a part of your current team.</strong></h1
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<?php include ("_include/footer.php"); ?>
